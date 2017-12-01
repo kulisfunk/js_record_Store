4e230254697e30e96e93d100425a33418aff5632
kulisfunk <kulisfunk@hotmail.com>
2017-12-01 19:34:51 +0000

    added describe and created record store object in spec file

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -1,3 +1,11 @@
 var assert = require("assert");
 var Record = require("../record.js");
 var RecordStore = require("../record_store.js");
+
+
+describe("RecordStore tests", function(){
+
+  beforeEach(function(){
+    record_store1 = new RecordStore("DiscWorld", "Glasgow");
+  })
+})
