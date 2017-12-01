29d5e132c348331bb87d668b080a876d44acaf4b
kulisfunk <kulisfunk@hotmail.com>
2017-12-01 19:08:30 +0000

    created record in beforeEach

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -4,7 +4,16 @@ var record = require("../record.js")
 
 describe("Record Tests", function(){
 
-  it("should have an artist name", function(){});
+  var record;
+
+  beforeEach(function(){
+     record1 = new Record("Pink Floyd", "Animals", "Rock", 1500);
+
+  })
+
+  it("should have an artist name", function(){
+    assert.strictEqual()
+  });
   it("should have a title", function(){});
   it("should have a genre", function(){});
   it("should have a price", function(){});
