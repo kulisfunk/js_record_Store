4cb46f75d24f9221c766cd7b4e0f577ade458d71
kulisfunk <kulisfunk@hotmail.com>
2017-12-01 20:28:43 +0000

    created test to check for record details returned as string

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -31,4 +31,9 @@ describe("RecordStore tests", function(){
       record_store1.addRecord(record2);
       assert.strictEqual(record_store1.inventory.length, 2);
     });
+    it("should return record properties as a string", function(){
+      record_store1.addRecord(record1);
+      var result = record_store1.details("Animals");
+      assert.strictEqual(result, "Artist: Pink Floyd, title: Animals, genre: Rock, price: £15");
+    });
 })
