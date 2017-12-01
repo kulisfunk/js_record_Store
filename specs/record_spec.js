2e27d6f74de7644b25f71fd665cac55f0e6a048c
kulisfunk <kulisfunk@hotmail.com>
2017-12-01 19:00:27 +0000

    four IT added for record spec

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -3,5 +3,9 @@ var record = require("../record.js")
 
 
 describe("Record Tests", function(){
-  
+
+  it("should have an artist name", function(){});
+  it("should have a title", function(){});
+  it("should have a genre", function(){});
+  it("should have a price", function(){});
 })
