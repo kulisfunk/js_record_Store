9732537948ef80b45112db3bf250c610ef499e28
kulisfunk <kulisfunk@hotmail.com>
2017-12-01 19:37:29 +0000

    added two record objects to record store spec file

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -7,5 +7,9 @@ describe("RecordStore tests", function(){
 
   beforeEach(function(){
     record_store1 = new RecordStore("DiscWorld", "Glasgow");
+    record1 = new Record("Pink Floyd", "Animals", "Rock", 1500);
+    record2 = new Record("Underworld", "Beaucoup Fish", "Electronic", 1300);
+
+    
   })
 })
