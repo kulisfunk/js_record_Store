450b68dfc6ba0b837d841c4641d0bc11496e9363
kulisfunk <kulisfunk@hotmail.com>
2017-12-01 19:49:41 +0000

    has name test passes

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -9,12 +9,14 @@ describe("RecordStore tests", function(){
   var record2;
 
   beforeEach(function(){
-    record_store1 = new RecordStore("DiscWorld", "Glasgow");
+    record_store1 = new RecordStore("Disc World", "Glasgow");
     record1 = new Record("Pink Floyd", "Animals", "Rock", 1500);
     record2 = new Record("Underworld", "Beaucoup Fish", "Electronic", 1300);
   })
 
-    it("should have a name", function(){});
+    it("should have a name", function(){
+      assert.strictEqual(record_store1.name, "Disc World");
+    });
     it("should have a city", function(){});
     it("should have a balance", function(){});
     it("should have empty inventory", function(){});
