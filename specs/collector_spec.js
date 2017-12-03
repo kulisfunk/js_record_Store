42f7ed2c11ed4ac2b9b8286a545c3c35316e763f
kulisfunk <kulisfunk@hotmail.com>
2017-12-03 20:07:00 +0000

    added before each to spec file

--- specs/collector_spec.js
+++ specs/collector_spec.js
@@ -2,3 +2,24 @@ var assert = require("assert");
 var RecordStore = require("../record_store.js");
 var Record = require("../record.js");
 var Collector = require("../collector.js");
+
+
+
+describe("Collector Tests", function(){
+  var record_store1;
+  var record1;
+  var record2;
+  var collector1;
+
+
+
+  beforeEach(function(){
+
+    record_store1 = new RecordStore("Disc World", "Glasgow");
+    record1 = new Record("U2", "Joshua Tree", "Rock", 1200);
+    record2 = new Record("Cocteau Twins", "Treasure", "Indie", 1400);
+    collector1 = new Collector("Martin", 2000);
+
+  })
+  
+})
